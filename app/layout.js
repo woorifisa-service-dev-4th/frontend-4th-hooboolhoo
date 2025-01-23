1e0828e5b3e1ea90b1993782833d06c3e49da694
jeongmin07262 <dlwjdals0726@gmail.com>
2025-01-23 14:02:03 +0900

    [fix] 메인 layout 백그라운드 색상 수정

--- app/layout.js
+++ app/layout.js
@@ -2,11 +2,11 @@ import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-[#f1f3f5]">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
-      <body className="w-[500px] min-h-screen mx-auto border-2 border-gray-500">
+      <body className="w-[500px] min-h-screen mx-auto bg-white">
         {children}
       </body>
     </html>
