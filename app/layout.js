b6f8f673306e7bb188e8fe5c869cd5dca8b04d0b
Yoon young chan <119031366+yyc0026@users.noreply.github.com>
2025-01-24 09:42:06 +0900

    searchBar 기능 작동 확인

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import Header from "./components/header";
 import Footer from "./components/footer";
 import { Provider } from "@/components/ui/provider";
+import "./globals.css"
 
 export default function RootLayout({ children }) {
   return (
