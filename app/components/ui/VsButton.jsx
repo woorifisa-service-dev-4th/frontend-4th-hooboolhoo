ef5fcd527c30227803b11e59964f72ef6f1e5fdf
eratchacha <papa021326@naver.com>
2025-01-24 11:49:57 +0900

    feat: resolve hydration error

---
+++ app/components/ui/VsButton.jsx
@@ -0,0 +1,11 @@
+// import { Button } from '@chakra-ui/react'
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const VsButton = () => {
+  return (
+        <Button size= "lg">hi</Button>
+  )
+}
+
+export default VsButton
--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
 import Tiger from "./components/Tiger/Tiger";
+import VsButton from "./components/ui/VsButton";
 
 export default function Home() {
   return (
-  <></>
+  <>
+  {/* <VsButton/> */}
+  </>
 );
 }
