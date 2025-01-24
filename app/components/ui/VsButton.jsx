30e0e42f42b795ee7f2e2797892fae1b8beb8547
eratchacha <papa021326@naver.com>
2025-01-24 14:32:44 +0900

    feat: implement vs button component

--- app/components/ui/VsButton.jsx
+++ app/components/ui/VsButton.jsx
@@ -1,5 +1,21 @@
+'use client'
+
 import { Button } from "@chakra-ui/react"
 
-const VsButton = () => {
-  return <Button>Button</Button>
+const VsButton = ({choice, text, size}) => {
+  return (
+  <Button
+    color = 'black'
+    fontSize= {size.fontSize}
+    borderRadius = {20}
+    px = {size.px}
+    py = {size.py}
+    bg = {choice === 'left' ? "#FFCC00":"#432616"}
+    onClick ={()=> {}}
+    >
+      {text}
+  </Button>
+  )
 }
+
+export default VsButton
--- app/page.js
+++ app/page.js
@@ -1,7 +1,14 @@
+import VsButton from './components/ui/VsButton'
+
+const test = {
+  fontSize: 48,
+  px : 45, // 가로축 패딩
+  py : 8, // 세로축 패딩
+}
 
 export default function Home() {
   return (
-  <>
-  </>
-);
+    <>
+    </>
+  );
 }
