6facabbcb8a985ed6fa8c4d42377b911342ee26e
Yoon young chan <119031366+yyc0026@users.noreply.github.com>
2025-01-24 15:08:48 +0900

    프로필 구현

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+"use client"; // 클라이언트 컴포넌트로 변환
+
 import Header from "./components/header";
 import Footer from "./components/footer";
 import { Provider } from "@/components/ui/provider";
@@ -8,9 +10,9 @@ export default function RootLayout({ children }) {
     <html lang="en" suppressHydrationWarning className="bg-[#f1f3f5]">
       <body className="w-[500px] min-h-screen mx-auto bg-white">
         <Provider>
-        <Header />
-        {children}
-        <Footer/>
+          <Header />
+          {children}
+          <Footer />
         </Provider>
       </body>
     </html>
