863408c4c112f8b7a7707ff5b4b210066038735b
jeongmin07262 <dlwjdals0726@gmail.com>
2025-02-02 17:36:06 +0900

    [feat] ChoiceCard 클릭 시 query 전달

--- app/choice-card/[cardId]/layout.js
+++ app/choice-card/[cardId]/layout.js
@@ -1,17 +1,26 @@
+"use client";
+
 import Image from "next/image";
+import { useSearchParams } from "next/navigation";
 import Comments from "@/app/components/Comments";
 import VsButton from "@/app/components/ui/VsButton";
 
 export default function Layout({ children }) {
+    const searchParams = useSearchParams();
+    const src = searchParams.get("src"); 
+    const title = searchParams.get("title");
+    const [leftText, rightText] = title.split(" vs ").map(text => text.trim()); 
+
     return (
         <section className="flex flex-col p-6">
             <div className="text-2xl font-bold items-center text-center mb-3">
-                당신이 선택한 최애는?
+                {title}
             </div>
 
+            {/* 이미지 섹션 */}
             <div className="relative w-full h-[320px] mb-6">
                 <Image 
-                    src="/images/test.jpg" 
+                    src={src} 
                     alt="선택한 결과" 
                     layout="fill"
                     objectFit="cover"
@@ -19,30 +28,26 @@ export default function Layout({ children }) {
                 />
             </div>
 
-            {/* 버튼 */}
+            {/* 버튼 섹션 */}
             <div className="flex justify-center gap-20">
                 <VsButton 
                     choice="left" 
-                    text="카리나" 
+                    text={leftText} 
                     size={{ fontSize: "20px", px: "60px", py: "30px" }} 
                 />
                 <VsButton 
                     choice="right" 
-                    text="윈터" 
-                    size={{ fontSize: "20px", px: "60px", py: "30px" }} 
+                    text={rightText} 
+                    size={{ fontSize: "20px", px: "60px", py: "30px" }}  
                 />
             </div>
 
             {/* 댓글 섹션 */}
             <div className="mt-10">
-                <div className="text-xl">
-                    선택 후 댓글을 남겨보세요
-                </div>
-                <div className="comment-section">  {/* 이 부분에 별도의 스타일 추가 */}
-                    <Comments choice="right" text="내 최애는 당연히 카리나지!" />
-                    <Comments choice="left" text="내 최애는 당연히 윈터지!" />
-                    <Comments choice="right" text="내 최애는 당연히 카리나지!" />
-                    <Comments choice="left" text="내 최애는 당연히 윈터지!" />
+                <div className="text-xl">선택 후 댓글을 남겨보세요</div>
+                <div className="comment-section">
+                    <Comments choice="right" text={`당연히 ${rightText}지!`} />
+                    <Comments choice="left" text={`당연히 ${leftText}지!`} />
                 </div>
             </div>
 
--- app/choice-card/page.js
+++ app/choice-card/page.js
@@ -7,24 +7,30 @@ import MakerButton from "../components/test-maker-button/makerButton";
 
 export default function Choice() {
   const [cards, setCards] = useState([
-    { id: "1", src: "/images/test.jpg", category: "❤️", title: "카리나 vs 윈터", subTitle: "당신의 최애를 골라주세요!" },
-    { id: "2", src: "/images/test.jpg", category: "❤️", title: "카리나 vs 윈터", subTitle: "당신의 최애를 골라주세요!" },
-    { id: "3", src: "/images/test.jpg", category: "❤️", title: "카리나 vs 윈터", subTitle: "당신의 최애를 골라주세요!" },
+    { id: "1", src: "/images/test1.jpg", category: "❤️", title: "카리나 vs 윈터", subTitle: "당신의 최애를 골라주세요!" },
+    { id: "2", src: "/images/test2.jpg", category: "🚗", title: "카공하기 vs 집공하기", subTitle: "당신의 최애를 골라주세요!" },
+    { id: "3", src: "/images/test3.jpg", category: "🐶", title: "강아지 vs 고양이", subTitle: "당신의 최애를 골라주세요!" },
+    { id: "4", src: "/images/test4.jpg", category: "🎬", title: "드라마 vs 예능", subTitle: "당신의 최애를 골라주세요!" },
+    { id: "5", src: "/images/test5.jpg", category: "🍜", title: "짜장 vs 짬뽕", subTitle: "당신의 최애를 골라주세요!" },
   ]);
 
   return (
     <div className="grid grid-cols-2 gap-10 m-8">
       <MakerButton />
       {cards.map((card, index) => (
-        <Link href={`/choice-card/${card.id}`} key={card.id}>
-          <ChoiceCard
-            id={card.id}
+        <Link
+          key={card.id}
+          href={{
+            pathname: `/choice-card/${card.id}`,
+            query: { src: card.src, title: card.title }, 
+          }}
+        >
+          <ChoiceCard  id={card.id}
             src={card.src}
             category={card.category}
             title={card.title}
             subTitle={card.subTitle}
-            borderColor={index % 2 === 0 ? "border-[#FF0000]" : "border-[#FFCC00]"}
-          />
+            borderColor={index % 2 === 0 ? "border-[#FF0000]" : "border-[#FFCC00]"} />
         </Link>
       ))}
     </div>
